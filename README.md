f22158cf9e2737d597451af17f32d15972bc7d53
Steve Thompson <writetome51@gmail.com>
2020-08-05 21:50:16 -0600

    updated to be ecmascript module Committing version 2.0.0

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Removes items, identified by `indexes`, from `array`.
 `indexes` can be negative or positive.
 
 ## Examples
-```
+```js
 let arr = [1,2,3,4,5,6,7];
 
 removeByIndexes([-1, 0, 1], arr);
 // arr is now [3,4,5,6]
 
-let arr = [10,20,30,40,50,60,70];
+arr = [10,20,30,40,50,60,70];
 
 removeByIndexes([2, 4, 6], arr);
 // arr is now [10,20,40,60]
@@ -20,10 +20,6 @@ removeByIndexes([2, 4, 6], arr);
 `npm i  @writetome51/array-remove-by-indexes`
 
 ## Loading
-```
-// if using TypeScript:
+```js
 import {removeByIndexes} from '@writetome51/array-remove-by-indexes';
-// if using ES5 JavaScript:
-var removeByIndexes = 
-    require('@writetome51/array-remove-by-indexes').removeByIndexes;
 ```
--- dist/tests.js
+++ dist/tests.js
@@ -1,96 +1,95 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("./index");
-var arrays_match_1 = require("@writetome51/arrays-match");
-var arr = ['he', 'llo', 'bay', 'bayyyy'];
+import { removeByIndexes } from './index.js';
+import { arraysMatch } from '@writetome51/arrays-match';
+
+let arr = ['he', 'llo', 'bay', 'bayyyy'];
+
+
 // Test 1: Make sure 0 removes first item:
-index_1.removeByIndexes([0], arr);
-if (arr[0] === 'llo')
-    console.log('test 1 passed');
-else
-    console.log('test 1 failed');
+removeByIndexes([0], arr);
+if (arr[0] === 'llo') console.log('test 1 passed');
+else console.log('test 1 FAILED');
+
+
 // Test 2: Make sure -1 removes last item:
-index_1.removeByIndexes([-1], arr);
-if (arr[arr.length - 1] === 'bay')
-    console.log('test 2 passed');
-else
-    console.log('test 2 failed');
+removeByIndexes([-1], arr);
+if (arr[arr.length - 1] === 'bay') console.log('test 2 passed');
+else console.log('test 2 FAILED');
+
+
 // Test 3: Make sure 0 and 2 removes first and third items:
 arr = ['he', 'llo', 'zz'];
-index_1.removeByIndexes([0, 2], arr);
-if (arr.length === 1 && arr[0] === 'llo')
-    console.log('test 3 passed');
-else
-    console.log('test 3 failed');
+removeByIndexes([0, 2], arr);
+if (arr.length === 1 && arr[0] === 'llo') console.log('test 3 passed');
+else console.log('test 3 FAILED');
+
+
 // Test 4: Make sure -2 removes second to last item and 1 removes second item:
 arr = ['he', 'llo', 'zz', 'gg', 'cc', 'aa'];
-index_1.removeByIndexes([-2, 1], arr);
-if (arrays_match_1.arraysMatch(arr, ['he', 'zz', 'gg', 'aa']))
-    console.log('test 4 passed');
-else
-    console.log('test 4 failed');
+removeByIndexes([-2, 1], arr);
+if (arraysMatch(arr, ['he', 'zz', 'gg', 'aa'])) console.log('test 4 passed');
+else console.log('test 4 FAILED');
+
+
 // Test 5: Make sure -6 removes first item and 5 removes last item:
 arr = ['he', 'llo', 'zz', 'gg', 'cc', 'aa'];
-index_1.removeByIndexes([-6, 5], arr);
-if (arrays_match_1.arraysMatch(arr, ['llo', 'zz', 'gg', 'cc']))
-    console.log('test 5 passed');
-else
-    console.log('test 5 failed');
+removeByIndexes([-6, 5], arr);
+if (arraysMatch(arr, ['llo', 'zz', 'gg', 'cc'])) console.log('test 5 passed');
+else console.log('test 5 FAILED');
+
+
 // Test 5A: Make sure -6 removes first item and -1 removes last item:
 arr = ['he', 'llo', 'zz', 'gg', 'cc', 'aa'];
-index_1.removeByIndexes([-6, -1], arr);
-if (arrays_match_1.arraysMatch(arr, ['llo', 'zz', 'gg', 'cc']))
-    console.log('test 5A passed');
-else
-    console.log('test 5A failed');
+removeByIndexes([-6, -1], arr);
+if (arraysMatch(arr, ['llo', 'zz', 'gg', 'cc'])) console.log('test 5A passed');
+else console.log('test 5A FAILED');
+
+
 // Test 6: Make sure -7 triggers error:
 arr = ['he', 'llo', 'zz', 'gg', 'cc', 'aa'];
-var errorTriggered = false;
+let errorTriggered = false;
 try {
-    index_1.removeByIndexes([-7], arr);
+    removeByIndexes([-7], arr);
 }
 catch (e) {
     errorTriggered = true;
 }
-if (errorTriggered)
-    console.log('test 6 passed');
-else
-    console.log('test 6 failed');
+if (errorTriggered) console.log('test 6 passed');
+else console.log('test 6 FAILED');
+
+
 // Test 7: Make sure 6 triggers error:
 errorTriggered = false;
 arr = ['he', 'llo', 'zz', 'gg', 'cc', 'aa'];
 try {
-    index_1.removeByIndexes([6], arr);
+    removeByIndexes([6], arr);
 }
 catch (e) {
     errorTriggered = true;
 }
-if (errorTriggered)
-    console.log('test 7 passed');
-else
-    console.log('test 7 failed');
+if (errorTriggered) console.log('test 7 passed');
+else console.log('test 7 FAILED');
+
+
 // Test 8: Make sure string '1' triggers error:
 errorTriggered = false;
 arr = ['he', 'llo', 'zz', 'gg', 'cc', 'aa'];
 try {
-    index_1.removeByIndexes(['1'], arr);
+    removeByIndexes(['1'], arr);
 }
 catch (e) {
     errorTriggered = true;
 }
-if (errorTriggered)
-    console.log('test 8 passed');
-else
-    console.log('test 8 failed');
+if (errorTriggered) console.log('test 8 passed');
+else console.log('test 8 FAILED');
+
+
 // Test 9: Make sure object in second argument triggers error:
 errorTriggered = false;
 try {
-    index_1.removeByIndexes([0], {});
+    removeByIndexes([0], {});
 }
 catch (e) {
     errorTriggered = true;
 }
-if (errorTriggered)
-    console.log('test 9 passed');
-else
-    console.log('test 9 failed');
+if (errorTriggered) console.log('test 9 passed');
+else console.log('test 9 FAILED');
